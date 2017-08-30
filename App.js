63529997edff71a72b31c76c02641a5ba1b9992c
Jie Wei <email2jie@gmail.com>
2017-08-29 21:41:43 -0700

    added common files

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+render() {
+  return (
+    <View>
+     tech stack! 
+    </View>
+  )
+}
+
+
+}
+
+const styles = {
+};
+
+export default App;
