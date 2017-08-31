90f32814e427118f3907f718736bfa955219d72c
Jie Wei <email2jie@gmail.com>
2017-08-30 23:48:24 -0700

    completed app

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
+
 class ListView extends Component {
+  componentWillUpdate() {
+    const CustomLayoutAnimation = {
+      duration: 50,
+      create: {
+        type: LayoutAnimation.Types.linear,
+        property: LayoutAnimation.Properties.opacity,
+      },
+      update: {
+        type: LayoutAnimation.Types.curveEaseInEaseOut,
+      },
+    };
 
-  render() {
-    const { titleStyle } = styles;
-    const { id, title } = this.props.library;
-    console.log(this.props);
+    LayoutAnimation.configureNext(CustomLayoutAnimation);
+  }
+  renderDescription() {
+    const { library, expanded } = this.props;
 
-    return (
-      <TouchableWithoutFeedback
-        onPress={() => this.props.selectLibrary(id)}
-      >
-        <View>
-          <CardSection>
-            <Text style={titleStyle}>{title}</Text>
-          </CardSection>
-        </View>
-      </TouchableWithoutFeedback>
-      )
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.description}</Text>
+        </CardSection>
+        );
+}
+  }
+
+render() {
+  const { titleStyle } = styles;
+  const { id, title } = this.props.library;
+  const { expanded, selectLibrary } = this.props;
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => { expanded ? selectLibrary(null) : selectLibrary(id)}} //toggle description
+    >
+      <View>
+        <CardSection>
+          <Text style={titleStyle}>{title}</Text>
+        </CardSection>
+        {this.renderDescription()}
+
+      </View>
+    </TouchableWithoutFeedback>
+    )
 }
 }
 
@@ -33,8 +61,9 @@ const styles = {
 
 };
 
-const mapStateToProps = state => {
-  return { selection: state.selectedLibraryId }
+const mapStateToProps = (state, ownProps) => { //ownProps is the props of the component
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded }
 }
 
 export default connect(mapStateToProps, actions)(ListView);
